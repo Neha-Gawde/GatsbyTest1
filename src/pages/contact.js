19e9fc0d70261b5479522fa948698112f6223db6
unknown <nehagawde931999@gmail.com>
2022-01-13 13:38:30 +0530

    created one new page and learn about page-data.json file

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ContactPage() {
+    return (
+        <div>
+           <h2>Contact</h2> 
+           <p>This is an contact page</p>
+        </div>
+    )
+}
+
+export default ContactPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ function IndexPage() {
       <h2>Gatsby Learning....</h2>
       <h2>About page<a href="/about" target={"_blank"}>Click here</a></h2>
       <h2>Blog page<a href="/blog" target={"_blank"}>Click here</a></h2>
+      <h2>Contact page<a href="/contact" target={"_blank"}>Click here</a></h2>
     </div>
   )
 };
