f125a9f88b729e68ea9f27eb9f6c3e23ce584e10
unknown <nehagawde931999@gmail.com>
2022-01-17 11:46:22 +0530

    changes in layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ function Header() {
                 <ul>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About Us</Link></li>
-                    <li><Link to="/about">Blog Post</Link></li>
+                    <li><Link to="/blog">Blog Post</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
                 </ul>
             </nav>
